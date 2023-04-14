7fb47b59713ce443d227430faee5f6cb3cf19199
ulira <kagovelidieudonne@gmail.com>
2023-04-14 10:13:52 +0300

    web

--- app/src/main/java/com/example/calcintentweb/web.kt
+++ app/src/main/java/com/example/calcintentweb/web.kt
@@ -1,11 +1,26 @@
 package com.example.calcintentweb
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 
 class web : AppCompatActivity() {
+    lateinit var mywebview:WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
+        mywebview=findViewById(R.id.webView)
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun webview(){
+        mywebview.webViewClient= WebViewClient()
+        mywebview.apply {
+            loadUrl("https://www.instagram.com/")
+            settings.javaScriptEnabled=true
+            settings.safeBrowsingEnabled=true
+        }
     }
 }
